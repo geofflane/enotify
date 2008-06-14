696042a87e9ec0f9db013cf93149ed8a0b4c625b
Geoff Lane <geoff@zorched.net>
2008-06-14 11:17:43 -0500

    Fix deployment and path to mysql socket Signed-off-by: Geoff Lane <geoff@zorched.net>

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,10 +7,14 @@ set :deploy_to, "/var/www/enotify"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
+default_run_options[:pty] = true
 set :scm, :git
 set :repository, "geoff@zorched.net:enotify.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :ssh_options, { :forward_agent => true }
 
-role :app, "enotify.zorched.net"
-role :web, "enotify.zorched.net"
-role :db,  "enotify.zorched.net", :primary => true
+role :app, "zorched.net"
+role :web, "zorched.net"
+role :db,  "zorched.net", :primary => true
 
