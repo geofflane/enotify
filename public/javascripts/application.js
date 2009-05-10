bc3322030d0734344cd027ace4e81aff28a64a56
Geoff Lane <geoff@zorched.net>
2009-05-09 22:13:40 -0500

    Fix some formatting and popup views. Signed-off-by: Geoff Lane <geoff@zorched.net>

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function createMarker(latitude, longitude, id) {
+	var marker = new GMarker(new GLatLng(latitude, longitude));
+	marker.value = id;
+	GEvent.addListener(marker, "click", function() {
+    	new Ajax.Request('/incidents/' + id, {asynchronous:true, evalScripts:true, method:'get'}); 
+    	return false;
+    });
+  	return marker;
+}
