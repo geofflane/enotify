28d8faf100ac74e15876b7aeda55fb47d1008722
Geoff Lane <geoff@zorched.net>
2009-05-13 20:43:15 -0500

    Add GPL Add Readme Freeze Gems and Rails for easier deployment.
    Signed-off-by: Geoff Lane <geoff@zorched.net>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+
 class UsersController < ApplicationController
   layout 'sessions'
   
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,20 @@
+# Milwaukee Enotify Deconstructed
+# Copyright (C) 2009 Geoff Lane <geoff@zorched.net>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # Defines named roles for users that may be applied to
 # objects in a polymorphic fashion. For example, you could create a role
 # "moderator" for an instance of a model (i.e., an object), a model class,
