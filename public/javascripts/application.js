0fdffae0e02eb0535b3aa989739ddc37a417cca2
Geoff Lane <geoff@zorched.net>
2009-07-27 20:59:04 -0500

    Fix createMarker to work on a non-root hosted webapp by passing the URL instead of just the id of the Incident.
    Signed-off-by: Geoff Lane <geoff@zorched.net>

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-function createMarker(latitude, longitude, id) {
+function createMarker(link, latitude, longitude) {
 	var marker = new GMarker(new GLatLng(latitude, longitude));
-	marker.value = id;
+	marker.value = link;
 	GEvent.addListener(marker, "click", function() {
-    	new Ajax.Request('/incidents/' + id, {asynchronous:true, evalScripts:true, method:'get'}); 
+    	new Ajax.Request(link, {asynchronous:true, evalScripts:true, method:'get'}); 
     	return false;
     });
   	return marker;
