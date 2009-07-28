34b14290b279e92a93b75040384fce0501637984
Geoff Lane <geoff@zorched.net>
2009-07-27 20:21:57 -0500

    Production config fixes Signed-off-by: Geoff Lane <geoff@zorched.net>

--- config/initializers/patch_for_mongrel.rb
+++ config/initializers/patch_for_mongrel.rb
@@ -1,7 +1,7 @@
 module ActionController
     class AbstractRequest < ActionController::Request
 	def self.relative_url_root=(path)
-            ActionController::Base.relative_url_root=(path)
+      ActionController::Base.relative_url_root=(path)
 	end
 	def self.relative_url_root
 	    ActionController::Base.relative_url_root
